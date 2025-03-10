d5c07c12a617ff100741762f33b4609cfe384bab
ezequiel-source <victor.arnault+github@gmail.com>
2025-03-10 17:05:52 -0300

    arquivos

---
+++ barramento-de-eventos/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.put("/lembretes", async (req, res) => {
+    contador++;
+    const { texto } = req.body;
+    lembretes[contador] = {
+        contador,
+        texto,
+    };
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        },
+    });
+    res.status(201).send(lembretes[contador]);
+});
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body;
+    axios.post('http://localhost:4000/eventos', evento);
+    axios.post('http://localhost:5000/eventos', evento);
+    res.status(200).send({ mssg: "ok"});
+});
+
+app.listen(10000, () => {
+    console.log('Barramento de eventos. Porta 10000.');
+});
