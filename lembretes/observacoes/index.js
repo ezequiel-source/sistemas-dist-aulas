01279b38f4ce695d2a9f67a072fa5a41f2e6ed6a
ezequiel-source <victor.arnault+github@gmail.com>
2025-03-10 17:49:04 -0300

    index.js observacoes

--- lembretes/observacoes/index.js
+++ lembretes/observacoes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -7,13 +8,19 @@ app.use(bodyParser.json());
 const observacoesPorLembreteId = {};
 const { v4: uuidv4 } = require('uuid');
 
-app.put('/lembretes/:id/observacoes', (req, res) => {
+app.put('/lembretes/:id/observacoes', async (req, res) => {
   const idObs = uuidv4();
   const { texto } = req.body;
 
   const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
   observacoesDoLembrete.push({ id: idObs, texto});
   observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+  await axios.post('http://localhost:10000/eventos', {
+    tipo: "ObservacaoCriada",
+    dados: {
+      id: idObs, texto, lembreteId: req.params.id
+    }
+  })
   res.status(201).send(observacoesDoLembrete);
 });
 
