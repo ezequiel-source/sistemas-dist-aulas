3c94444c496b7895fbc255b2f1f50d4a18e195a7
ezequiel-source <victor.arnault+github@gmail.com>
2025-03-23 20:39:12 -0300

    "correções"

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 app = express();
+app.use(bodyParser.json());
+
 lembretes = {};
 contador = 0;
 
 app.get('/lembretes', (req, res) => {
   res.send(lembretes);
 });
-app.put('/lembretes', (res, req) => {
+app.put('/lembretes', (req, res) => {
   contador++;
   const { texto } = req.body;
   lembretes[contador] = {
@@ -16,7 +18,7 @@ app.put('/lembretes', (res, req) => {
   res.status(201).send(lembretes[contador]);
 });
 
-app.post("/eventos", (res, req) => {
+app.post("/eventos", (req, res) => {
   console.log(req.body);
   res.status(200).send({ msg: "ok"});
 });
