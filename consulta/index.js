725272435df9cab3db9465f14eb920d83248b1ee
ezequiel-source <victor.arnault+github@gmail.com>
2025-03-23 20:12:42 -0300

    "get e post"

--- consulta/index.js
+++ consulta/index.js
@@ -4,8 +4,24 @@ app.use(express.json());
 
 const baseConsulta = {};
 
-app.get("/lembretes", (req, res) => {});
+const funcoes = {
+    LembreteCriado: (lembrete) => {
+        baseConsulta[lembrete.contador] = lembrete;
+    },
+    ObservacaoCriada: (observacao) => {
+        const observacoes = baseConsulta[observacao.lembreteId]["observacoes"] || [];
+        observacoes.push(observacao);
+        baseConsulta[observacao.lembreteId]["observacoes"] = observacoes;
+    }
+};
 
-app.post("/eventos", (req, res) => {});
+app.get("/lembretes", (req, res) => {
+    res.status(200).send(baseConsulta);
+});
+
+app.post("/eventos", (req, res) => {
+    funcoes[req.body.tipo](req.body.dados);
+    res.status(200).send(baseConsulta);
+});
 
 app.listen(6000, () => console.log("Consultas. Porta 6000"));
